cb85e8cff3468b8b3233eaf834fcf48ecd11892d
Johnson, Randall (NIH/NCI) [C] <johnsonra@mail.nih.gov>
2017-05-03 20:58:07 -0400

    Start of code for Sequence Analysis lab

---
+++ Discussions/16_SequenceAnalysis.R
@@ -0,0 +1,61 @@
+# RNA seq lab
+# BIFX 553
+# Hood College
+
+# For this lab we will be following the rnaseqGene workflow found at https://www.bioconductor.org/help/workflows/rnaseqGene/
+# some parts have been skipped, but you can follow along on the webpage.
+
+
+######### load/install packages where we want them #########
+# start by resetting the default library path for installation and loading packages
+.libPaths(c("/Volumes/MASTER/MacLibs", .libPaths())) # for OS X
+# .libPaths(c("", .libpaths())) # for Windows
+
+source("https://bioconductor.org/biocLite.R")
+
+install.required <- function(pkg, bioc = FALSE)
+{
+    failedLoad <- eval(parse(text = paste0("!require(", pkg, ")")))
+
+    if(failedLoad & !bioc)
+    {
+        install.packages(pkg)
+        eval(parse(text = paste0('library(', pkg, ')')))
+    }
+
+    if(failedLoad & bioc)
+    {
+        biocLite(pkg)
+        eval(parse(text = paste0('library(', pkg, ')')))
+    }
+}
+
+install.required("airway", bioc = TRUE)
+install.required("Rsamtools", bioc = TRUE)
+install.required("GenomicAlignments", bioc = TRUE)
+install.required("GenomicFeatures", bioc = TRUE)
+install.required("DESeq2", bioc = TRUE)
+install.required("AnnotationDbi", bioc = TRUE)
+install.required("org.Hs.eg.db", bioc = TRUE)
+install.required("ReportingTools", bioc = TRUE)
+install.required("Gviz", bioc = TRUE)
+install.required("sva", bioc = TRUE)
+install.required("fission", bioc = TRUE)
+install.required("tidyverse")
+install.required("magrittr")
+install.required("ggplot2")
+install.required("pheatmap")
+install.required("RColorBrewer")
+install.required("genefilter")
+
+
+######### Now run the tutorial #########
+
+# skip parts that depend on STAR and samtools
+
+########################
+# Load and format Data #
+########################
+
+# Figure out where our data files are stored
+indir <- system.file("extdata", package="airway", mustWork=TRUE)
